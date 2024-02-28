ce35e879820f6be7dda92ed0b9288d2979cfaec1
Hendrik Saly <hendrik.saly@eliatra.com>
2024-02-28 08:09:45 +0100

    Styling corrections, logo added

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-bundle exec jekyll serve --host localhost --port 4000 --incremental --livereload --open-url
+rm -rf _site/ .jekyll-*
+bundle install
+JEKYLL_LINK_CHECKER=internal bundle exec jekyll serve --host localhost --port 4000 --incremental --livereload --open-url
