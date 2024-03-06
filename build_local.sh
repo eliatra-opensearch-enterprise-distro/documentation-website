405c9a1610ca3bf1355f66c1d7057e169685f3a9
Hendrik Saly <hendrik.saly@eliatra.com>
2024-03-06 14:25:03 +0100

    Striked "Enterprise", corrected favicon

--- build_local.sh
+++ build_local.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# from your command line
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
 rm -rf _site/ .jekyll-*
 bundle install
 JEKYLL_LINK_CHECKER=internal bundle exec jekyll serve --host localhost --port 4000 --incremental --livereload --open-url
